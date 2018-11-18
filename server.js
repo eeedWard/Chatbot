8e370f7976329a26cf9d7d53e35fba7aeaf82d51
eeedWard <36623400+eeedWard@users.noreply.github.com>
2018-11-18 21:49:33 +0100

    Small changes

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express()
 const path = require('path');
 
-const port = process.env.PORT || 5656;
+const port = process.env.PORT || 5656; // use defined port or port 5656 if not defined
 const ViewFiles = path.join(__dirname, '/View/');
 
 app.get('/',function(req,res){
-  res.sendFile(ViewFiles + 'Demo.html');
+  res.sendFile(ViewFiles + 'home.html'); // home page
 });
 
 app.get('/Prof_Marcus',function(req,res){
-  res.sendFile(ViewFiles + 'Prof_Marcus.html');
+  res.sendFile(ViewFiles + 'Prof_Marcus.html'); // html page initially displayed on the chatbot iframe
 });
 
 app.get('/sleeping_img',function(req,res){
-  res.sendFile(ViewFiles + 'sleeping.jpg');
+  res.sendFile(ViewFiles + 'sleeping.jpg'); // image to be initially displayed on the chatbot iframe
 });
 
 app.listen(port, () => {
     console.log('http://localhost: ' + port)
-})
+});
